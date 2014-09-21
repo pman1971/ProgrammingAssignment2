c6ff615c3e473b675c864a3b4fc5fd8a181fc77e
Petros Koulouris <petros.koulouris@gmail.com>
2014-09-21 22:03:41 +1000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Overall objective is to be able to apply inverse matrix
+## 
 
 ## Write a short comment describing this function
 
